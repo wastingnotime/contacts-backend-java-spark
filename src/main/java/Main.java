5bccff832bbedeebcf1ed2f93f37462e3003f5ec
hriccio <hriccio@wastingnotime.org>
2024-06-24 09:11:36 -0300

    included path /contacts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,57 +19,61 @@ public class Main {
         contactList.add(new Contact(UUID.randomUUID().toString(), "Albert", "Einstein", "1111-1111"));
         contactList.add(new Contact(UUID.randomUUID().toString(), "Marie", "Curie", "2222-1111"));
 
-        post("/", (request, response) -> {
-            Contact contact = gson.fromJson(request.body(), Contact.class);
-            contact.setId(UUID.randomUUID().toString());
-            contactList.add(contact);
-            response.status(201);
-            //response.header("Location", "http://" + request.host() + request.uri() + "/" + contact.getId());
-            response.header("Location", "/" + contact.getId());
-            return "";
-        });
+        path("/contacts", () -> {
 
-        get("/", (request, response) -> {
-            response.status(200);
-            response.type("application/json");
-            return contactList;
-        },gson::toJson);
+            post("", (request, response) -> {
+                Contact contact = gson.fromJson(request.body(), Contact.class);
+                contact.setId(UUID.randomUUID().toString());
+                contactList.add(contact);
+                response.status(201);
+                //response.header("Location", "http://" + request.host() + request.uri() + "/" + contact.getId());
+                response.header("Location", "/" + contact.getId());
+                return "";
+            });
 
-        get("/:id", (request, response) -> {
-            String id = request.params(":id");
-            response.status(200);
-            response.type("application/json");
-            return contactList.stream().filter(contact ->  contact.getId().equals(id)).findFirst();
-        },gson::toJson);
+            get("", (request, response) -> {
+                response.status(200);
+                response.type("application/json");
+                return contactList;
+            }, gson::toJson);
 
-        put("/:id", (request, response) -> {
-            String id = request.params(":id");
-            Contact contact = gson.fromJson(request.body(), Contact.class);
+            get(":id", (request, response) -> {
+                String id = request.params(":id");
+                response.status(200);
+                response.type("application/json");
+                return contactList.stream().filter(contact -> contact.getId().equals(id)).findFirst();
+            }, gson::toJson);
 
-            Optional<Contact> optionalContact = contactList.stream().filter(c ->  c.getId().equals(id)).findFirst();
-            if (optionalContact.isEmpty()){
-                halt(404);
-            }
+            put(":id", (request, response) -> {
+                String id = request.params(":id");
+                Contact contact = gson.fromJson(request.body(), Contact.class);
 
-            Contact currentContact = optionalContact.get();
-            currentContact.setFirstName(contact.getFirstName());
-            currentContact.setLastName(contact.getLastName());
-            currentContact.setPhoneNumber(contact.getPhoneNumber());
+                Optional<Contact> optionalContact = contactList.stream().filter(c -> c.getId().equals(id)).findFirst();
+                if (optionalContact.isEmpty()) {
+                    halt(404);
+                }
 
-            response.status(204);
-            return "";
-        });
+                Contact currentContact = optionalContact.get();
+                currentContact.setFirstName(contact.getFirstName());
+                currentContact.setLastName(contact.getLastName());
+                currentContact.setPhoneNumber(contact.getPhoneNumber());
 
-        delete("/:id", (request, response) -> {
-            String id = request.params(":id");
-            Optional<Contact> optionalContact = contactList.stream().filter(c ->  c.getId().equals(id)).findFirst();
-            if (optionalContact.isEmpty()){
-                halt(404);
-            }
+                response.status(204);
+                return "";
+            });
 
-            contactList.remove(optionalContact.get());
-            response.status(204);
-            return "";
+            delete(":id", (request, response) -> {
+                String id = request.params(":id");
+                Optional<Contact> optionalContact = contactList.stream().filter(c -> c.getId().equals(id)).findFirst();
+                if (optionalContact.isEmpty()) {
+                    halt(404);
+                }
+
+                contactList.remove(optionalContact.get());
+                response.status(204);
+                return "";
+            });
+
         });
     }
 }
